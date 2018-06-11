52639a5fd1371fe5616b3e47939e34af932ea970
Kristen Chellis <peanutster@gmail.com>
2018-06-11 15:01:02 -0700

    follow lessons to add routes to App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,98 @@
+import com.google.gson.Gson;
+import dao.Sql2oFoodtypeDao;
+import dao.Sql2oRestaurantDao;
+import dao.Sql2oReviewDao;
+import models.Foodtype;
+import models.Restaurant;
+import models.Review;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+import static spark.Spark.*;
+
 public class App {
+
+    public static void main(String[] args) {
+        Sql2oFoodtypeDao foodtypeDao;
+        Sql2oRestaurantDao restaurantDao;
+        Sql2oReviewDao reviewDao;
+        Connection conn;
+        Gson gson = new Gson();
+
+        String connectionString = "jdbc:h2:~/jadle.db;INIT=RUNSCRIPT from 'classpath:db/create.sql'";
+        Sql2o sql2o = new Sql2o(connectionString, "", "");
+
+        restaurantDao = new Sql2oRestaurantDao(sql2o);
+        foodtypeDao = new Sql2oFoodtypeDao(sql2o);
+        reviewDao = new Sql2oReviewDao(sql2o);
+        conn = sql2o.open();
+
+        post("/restaurants/new", "application/json", (req, res) -> {
+            Restaurant restaurant = gson.fromJson(req.body(), Restaurant.class);
+            restaurantDao.add(restaurant);
+            res.status(201);
+            return gson.toJson(restaurant);
+        });
+
+        post("/foodtypes/new", "application/json", (req, res) -> {
+            Foodtype foodtype = gson.fromJson(req.body(), Foodtype.class);
+            foodtypeDao.add(foodtype);
+            res.status(201);
+            return gson.toJson(foodtype);
+        });
+
+        get("/restaurants/delete", "application/json", (req, res) -> {
+            restaurantDao.clearAll();
+            return gson.toJson(restaurantDao.getAll());
+        });
+
+        get("/foodtypes/delete", "application/json", (req, res) -> {
+            foodtypeDao.clearAll();
+            return gson.toJson(foodtypeDao.getAll());
+        });
+
+        get("/restaurants", "application/json", (req, res) -> {
+            return gson.toJson(restaurantDao.getAll());
+        });
+
+        get("/foodtypes", "application/json", (req, res) -> {
+            return gson.toJson(foodtypeDao.getAll());
+        });
+
+        get("/restaurants/:id", "application/json", (req, res) -> {
+            int restaurantId = Integer.parseInt(req.params("id"));
+            return gson.toJson(restaurantDao.findById(restaurantId));
+        });
+
+        get("/restaurants/:id/delete", "application/json", (req, res) -> {
+            int restaurantId = Integer.parseInt(req.params("id"));
+            restaurantDao.deleteById(restaurantId);
+            return gson.toJson(restaurantDao.getAll());
+        });
+
+        get("/foodtypes/:id/delete", "application/json", (req, res) -> {
+            int foodtypeId = Integer.parseInt(req.params("id"));
+            foodtypeDao.deleteById(foodtypeId);
+            return gson.toJson(foodtypeDao.getAll());
+        });
+
+        get("/restaurants/:id/reviews", "application/json", (req, res) -> {
+            int restaurantId = Integer.parseInt(req.params("id"));
+            return gson.toJson(reviewDao.getAllReviewsByRestaurant(restaurantId));
+        });
+
+        post("/restaurants/:restaurantId/reviews/new", "application/json", (req, res) -> {
+            int restaurantId = Integer.parseInt(req.params("restaurantId"));
+            Review review = gson.fromJson(req.body(), Review.class);
+
+            review.setRestaurantId(restaurantId); //we need to set this separately because it comes from our route, not our JSON input.
+            reviewDao.add(review);
+            res.status(201);
+            return gson.toJson(review);
+        });
+
+        //FILTERS
+        after((req, res) ->{
+            res.type("application/json");
+        });
+    }
 }
