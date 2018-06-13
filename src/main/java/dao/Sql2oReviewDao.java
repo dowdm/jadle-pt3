0b7b567a7c6f2df8661fef7355edb59bef7eaf57
Kristen Chellis and Matt Dowdney <kristen@>
2018-06-13 13:46:12 -0700

    add if statement to sort function to handle <2 reviews

--- src/main/java/dao/Sql2oReviewDao.java
+++ src/main/java/dao/Sql2oReviewDao.java
@@ -71,14 +71,16 @@ public class Sql2oReviewDao implements ReviewDao {
     @Override
     public List<Review> getAllReviewsByRestaurantIdSortedNewestToOldest(int restaurantId) {
         List<Review> reviews = getAllReviewsByRestaurant(restaurantId);
-        for(int j = 1; j <= reviews.size(); j++){
-            for (Integer index = 0; index < reviews.size() - 1; index++) {
-                Review review1 = reviews.get(index);
-                Review review2 = reviews.get(index + 1);
-                int compareResult = review1.compareTo(reviews.get(index + 1));
-                if (compareResult == -1) {
-                    reviews.set(index, review2);
-                    reviews.set(index + 1, review1);
+        if(reviews.size()>1){
+            for(int j = 1; j <= reviews.size(); j++){
+                for (Integer index = 0; index < reviews.size() - 1; index++) {
+                    Review review1 = reviews.get(index);
+                    Review review2 = reviews.get(index + 1);
+                    int compareResult = review1.compareTo(reviews.get(index + 1));
+                    if (compareResult == -1) {
+                        reviews.set(index, review2);
+                        reviews.set(index + 1, review1);
+                    }
                 }
             }
         }
