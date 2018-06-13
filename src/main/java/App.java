23e7727d84b77e453be75bbd4f6f819724819511
Kristen Chellis and Matt Dowdney <matt@>
2018-06-13 13:09:46 -0700

    add sorting methods and timestamp methods

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -10,6 +10,7 @@ import org.sql2o.Connection;
 import org.sql2o.Sql2o;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static spark.Spark.*;
@@ -88,11 +89,23 @@ public class App {
         post("/restaurants/:restaurantId/reviews/new", "application/json", (req, res) -> {
             int restaurantId = Integer.parseInt(req.params("restaurantId"));
             Review review = gson.fromJson(req.body(), Review.class);
-
+            review.setCreatedAt(); //I am new!
+            review.setFormattedCreatedAt();
             review.setRestaurantId(restaurantId); //we need to set this separately because it comes from our route, not our JSON input.
             reviewDao.add(review);
             res.status(201);
             return gson.toJson(review);
+        });
+
+        get("/restaurants/:id/sortedReviews", "application/json", (req, res) -> { //// TODO: 1/18/18 generalize this route so that it can be used to return either sorted reviews or unsorted ones.
+            int restaurantId = Integer.parseInt(req.params("id"));
+            Restaurant restaurantToFind = restaurantDao.findById(restaurantId);
+            List<Review> allReviews;
+            if (restaurantToFind == null){
+                throw new ApiException(404, String.format("No restaurant with the id: \"%s\" exists", req.params("id")));
+            }
+            allReviews = reviewDao.getAllReviewsByRestaurantSortedNewestToOldest(restaurantId);
+            return gson.toJson(allReviews);
         });
 
         get("/restaurants/:id/foodtypes", "application/json", (req, res) -> {
--- src/main/java/dao/Sql2oReviewDao.java
+++ src/main/java/dao/Sql2oReviewDao.java
@@ -4,6 +4,8 @@ import models.Review;
 import org.sql2o.Connection;
 import org.sql2o.Sql2o;
 import org.sql2o.Sql2oException;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class Sql2oReviewDao implements ReviewDao {
@@ -12,7 +14,7 @@ public class Sql2oReviewDao implements ReviewDao {
 
     @Override
     public void add(Review review) {
-        String sql = "INSERT INTO reviews (writtenby, content, rating, restaurantid) VALUES (:writtenBy, :content, :rating, :restaurantId)"; //if you change your model, be sure to update here as well!
+        String sql = "INSERT INTO reviews (writtenby, content, rating, restaurantid, createdAt) VALUES (:writtenBy, :content, :rating, :restaurantId, :createdAt)"; //if you change your model, be sure to update here as well!
         try (Connection con = sql2o.open()) {
             int id = (int) con.createQuery(sql, true)
                     .bind(review)
@@ -61,5 +63,35 @@ public class Sql2oReviewDao implements ReviewDao {
         } catch (Sql2oException ex) {
             System.out.println(ex);
         }
+    }
+
+    @Override
+    public List<Review> getAllReviewsByRestaurantSortedNewestToOldest(int restaurantId) {
+        List<Review> unsortedReviews = getAllReviewsByRestaurant(restaurantId);
+        List<Review> sortedReviews = new ArrayList<>();
+        int i = 1;
+        for (Review review : unsortedReviews){
+            int comparisonResult;
+            if (i == unsortedReviews.size()) { //we need to do some funky business here to avoid an arrayindex exception and handle the last element properly
+                if (review.compareTo(unsortedReviews.get(i-1)) == -1){
+                    sortedReviews.add(0, unsortedReviews.get(i-1));
+                }
+                break;
+            }
+
+            else {
+                if (review.compareTo(unsortedReviews.get(i)) == -1) { //first object was made earlier than second object
+                    sortedReviews.add(0, unsortedReviews.get(i));
+                    i++;
+                } else if (review.compareTo(unsortedReviews.get(i)) == 0) {//probably should have a tie breaker here as they are the same.
+                    sortedReviews.add(0, unsortedReviews.get(i));
+                    i++;
+                } else {
+                    sortedReviews.add(0, unsortedReviews.get(i)); //push the first object to the list as it is newer than the second object.
+                    i++;
+                }
+            }
+        }
+        return sortedReviews;
     }
 }
